bb3cc18dc476751dae8380887181907af6eb895f
kingrise2@gmail.com <kingrise2@gmail.com@c8f7cd85-c129-ca36-a003-54200d092c27>
2014-04-18 18:40:51 +0000

    adding butons for back

---  2048-game-imperia --username hristo_rv@abv.bg/src/menus/MainMenu.java
+++  2048-game-imperia --username hristo_rv@abv.bg/src/menus/MainMenu.java
@@ -1,13 +1,14 @@
 package menus;
 
 import initialization.InitializationActivity;
-import com.example.finalproject.R;
-import android.media.MediaPlayer;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.finalproject.R;
 
 public class MainMenu extends Activity {
 	public static MediaPlayer mpBackgroundSound;
@@ -18,14 +19,16 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_menu);
-		MainMenu.mpBackgroundSound = MediaPlayer.create(this, R.raw.fun_with_friends);
-		MainMenu.mpBackgroundSound.setVolume(0.5f, 0.5f);
+		MainMenu.mpBackgroundSound = MediaPlayer.create(this,
+				R.raw.fun_with_friends);
+		MainMenu.mpBackgroundSound.setVolume(0f, 0f);
 		if (!MainMenu.isMuted)
 			MainMenu.mpBackgroundSound.start();
 		MainMenu.mpBackgroundSound.setLooping(true);
 
 		Button newGameButton = (Button) findViewById(R.id.new_game_button);
 		Button aboutButton = (Button) findViewById(R.id.about_button);
+		Button exitGameButton = (Button) findViewById(R.id.exit_button);
 
 		newGameButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -47,6 +50,14 @@ public class MainMenu extends Activity {
 			}
 		});
 
+		exitGameButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!MainMenu.isMuted)
+					MainMenu.mpButtonClick.start();
+				finish();
+			}
+		});
 	}
 
 	@Override
